fd3f4f9883a79da872a9027c91743780a7cfa725
times132 <times132@gmail.com>
2020-01-15 02:10:38 +0900

    react 2-4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const iuLike = [
+    {
+      id: 1,
+      name: "IU",
+      image: "https://img.sbs.co.kr/newsnet/etv/upload/2019/10/11/30000635839_1280.jpg"
+    },
+    {
+      id: 2,
+      name: "fullmoon",
+      image: "https://i.pinimg.com/236x/cd/db/4a/cddb4af5193d707c327b83495810ea5f.jpg"
+    },
+    {
+      id: 3,
+      name: "jian",
+      image: "http://optimal.inven.co.kr/upload/2018/05/19/bbs/i13643437109.jpg"
+    }
+    ];
+
+function IU({name, picture}) {
+  return <div>
+    <h2>I like {name}</h2>
+    <img src={picture} alt={name}/>
+  </div>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <h1>Hello!!!!</h1>
+        {iuLike.map(iu => (
+            <IU key={iu.id} name={iu.name} picture={iu.image}/>
+            ))}
+      </div>
   );
 }
 
